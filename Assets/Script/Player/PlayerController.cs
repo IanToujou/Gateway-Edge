6ec6b979af32e1d8e37dac40c967fe3eff609505
IanToujou <ianbour2016@gmail.com>
2021-12-03 22:03:11 +0100

    Optimized movement

--- Assets/Script/Player/PlayerController.cs
+++ Assets/Script/Player/PlayerController.cs
@@ -3,14 +3,22 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
     
     [SerializeField] private float baseSpeed = 5.0f;
+    [SerializeField] private float maxSpeed = 1.0f;
     [SerializeField] private float rotationSpeed = 30.0f;
+    [SerializeField] private float boostAmount = 3f;
 
-    private Rigidbody rigidBody;
     private Camera cam;
+    private Rigidbody rb;
+    private bool boostKey;
 
     void Start() {
-        rigidBody = gameObject.GetComponent<Rigidbody>();
         cam = FindObjectOfType<Camera>();
+        rb = GetComponent<Rigidbody>();
+        boostKey = false;
+    }
+
+    void Update() {
+        bool boostKey = (Input.GetAxisRaw("Vertical") >= 0.5f);
     }
 
     void FixedUpdate() {
@@ -28,7 +36,15 @@ public class PlayerController : MonoBehaviour {
             transform.LookAt(pointToLook);
 
             Vector3 pointToMove = new Vector3(pointToLook.x, pointToLook.y, pointToLook.z);
-            transform.position = Vector3.MoveTowards(transform.position, pointToMove, baseSpeed * Time.deltaTime);
+
+            rb.velocity = (pointToMove.normalized * baseSpeed);
+            Debug.Log(pointToMove);
+            
+            /*if(boostKey) {
+                transform.position = Vector3.MoveTowards(transform.position, pointToMove, baseSpeed * Time.deltaTime + boostAmount);
+            } else {
+                transform.position = Vector3.MoveTowards(transform.position, pointToMove, baseSpeed * Time.deltaTime);
+            }*/
 
         }
 
