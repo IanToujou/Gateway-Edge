8f6e818199fcaf3815eccf8fa5a8ef36707ca478
= <ianbour2016@gmail.com>
2021-12-16 22:27:31 +0100

    Added brake mechanics

--- Assets/Script/Player/PlayerController.cs
+++ Assets/Script/Player/PlayerController.cs
@@ -5,19 +5,30 @@ public class PlayerController : MonoBehaviour {
     [SerializeField] private float baseSpeed;
     [SerializeField] private float rotationSpeed;
     [SerializeField] private float maxSpeed;
+    [SerializeField] private float minSpeed;
 
     private Camera cam;
     private Rigidbody rb;
     private float speedMultiplier;
+    private bool boosting;
+    private bool braking;
 
     void Start() {
         cam = FindObjectOfType<Camera>();
         rb = GetComponent<Rigidbody>();
         speedMultiplier = 1;
+        boosting = false;
+        braking = false;
     }
 
     void Update() {
         Vector3 moveInput = new Vector3(0, 0, 0);
+        Debug.Log(rb.velocity.magnitude);
+
+        //Update controls
+        boosting = (Input.GetAxisRaw("Vertical") >= 0.5f);
+        braking = (Input.GetAxisRaw("Vertical") <= -0.5f);
+
     }
 
     void FixedUpdate() {
@@ -32,19 +43,48 @@ public class PlayerController : MonoBehaviour {
 
             Vector3 rayPoint = cameraRay.GetPoint(rayLength);
             Vector3 pointToLook = new Vector3(rayPoint.x, transform.position.y, rayPoint.z);
-
             Vector3 pointToMove = Vector3.RotateTowards(rb.rotation.eulerAngles, pointToLook - transform.position, Mathf.PI / 2, 0.5f);
-
             Quaternion targetRotation = Quaternion.LookRotation(pointToMove);
 
             rb.rotation = Quaternion.Slerp(rb.rotation, targetRotation, 0.1f * rotationSpeed);
-            
-            //Check if the player magnitude is at maximum speed. If not, accelerate.
-            if(rb.velocity.magnitude < maxSpeed) {
-                rb.velocity = transform.TransformDirection(new Vector3(0, rb.velocity.y, baseSpeed + 0.1f * speedMultiplier));
-                speedMultiplier++;
+
+            //Check for any controls
+            if(boosting && !braking) {
+
+                //Check if the player magnitude is at maximum speed. If not, accelerate.
+                if(rb.velocity.magnitude <= maxSpeed - 0.1f) {
+                    rb.velocity = transform.TransformDirection(new Vector3(0, rb.velocity.y, baseSpeed + 0.2f * speedMultiplier));
+                    speedMultiplier++;
+                } else {
+                    rb.velocity = transform.TransformDirection(new Vector3(0, rb.velocity.y, maxSpeed));
+                }
+
+            } else if(braking && !boosting) {
+
+                //Check if the player magnitude is at minimum speed. If not, brake.
+                if(rb.velocity.magnitude >= minSpeed + 0.1f) {
+                    rb.velocity = transform.TransformDirection(new Vector3(0, rb.velocity.y, baseSpeed + 0.2f * speedMultiplier));
+                    speedMultiplier--;
+                } else {
+                    rb.velocity = transform.TransformDirection(new Vector3(0, rb.velocity.y, minSpeed));
+                }
+
             } else {
-                rb.velocity = transform.TransformDirection(new Vector3(0, rb.velocity.y, maxSpeed));
+                
+                //Turn back to base speed.
+                if(speedMultiplier > 1) {
+                    speedMultiplier--;
+                } else if(speedMultiplier < 1) {
+                    speedMultiplier++;
+                }
+
+                //Check if the player is near the base speed. If not, brake.
+                if(rb.velocity.magnitude > baseSpeed + 0.1f) {
+                    rb.velocity = transform.TransformDirection(new Vector3(0, rb.velocity.y, baseSpeed + 0.2f * speedMultiplier));
+                } else {
+                    rb.velocity = transform.TransformDirection(new Vector3(0, rb.velocity.y, baseSpeed));
+                }
+
             }
 
         }
