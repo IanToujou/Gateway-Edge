6c0848064cb2007d29b7bcdc6f6ca463178f450c
= <ianbour2016@gmail.com>
2022-01-11 22:15:55 +0100

    Added camera shake

---
+++ Assets/Script/Player/CameraShake.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using System.Collections;
+
+public class CameraShake : MonoBehaviour
+{
+	// Transform of the camera to shake. Grabs the gameObject's transform
+	// if null.
+	public Transform camTransform;
+	
+	// How long the object should shake for.
+	public float shakeDuration = 0f;
+	
+	// Amplitude of the shake. A larger value shakes the camera harder.
+	public float shakeAmount;
+	public float decreaseFactor;
+	
+	Vector3 originalPos;
+	
+	void Awake()
+	{
+		if (camTransform == null)
+		{
+			camTransform = GetComponent(typeof(Transform)) as Transform;
+		}
+	}
+	
+	void OnEnable()
+	{
+		originalPos = camTransform.localPosition;
+	}
+
+	void Update()
+	{
+		if (shakeDuration > 0)
+		{
+			camTransform.localPosition = originalPos + Random.insideUnitSphere * shakeAmount;
+			
+			shakeDuration -= Time.deltaTime * decreaseFactor;
+		}
+		else
+		{
+			shakeDuration = 0f;
+			camTransform.localPosition = originalPos;
+		}
+	}
+
+    public void Shake(float amount, float duration, float decrease) {
+
+        this.shakeAmount = amount;
+        this.shakeDuration = duration;
+        this.decreaseFactor = decrease;
+
+    }
+
+}
