2efd27c0617a12f0b7b71035494272c4922ba427
= <ianbour2016@gmail.com>
2022-01-17 23:57:04 +0100

    Added level selection background

--- Assets/Script/LevelSelectionUI/Zone/ZoneUI.cs
+++ Assets/Script/LevelSelectionUI/Zone/ZoneUI.cs
@@ -7,11 +7,22 @@ public class ZoneUI : MonoBehaviour {
     [SerializeField] private int zoneNumber;
     [SerializeField] private List<GameObject> levelButtons = new List<GameObject>();
 
+    private Camera playerCamera;
+    private ParticleSystem particles;
+    private ParticleSystem.MainModule particleMainModule;
+
     void Start() {
 
         SaveManager saveManager = SaveManager.GetInstance();
+        playerCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        particles = GameObject.Find("BackgroundParticles").GetComponent<ParticleSystem>();
+        particleMainModule = particles.main;
+
+        playerCamera.backgroundColor = new Color(0.2455351f, 0f, 0.3867925f, 0f);
+        particleMainModule.startColor = new Color(1f, 0f, 0.5132675f, 1f);
 
         for(int i = 1; i <= levelButtons.Count; i++) {
+            levelButtons[i-1].GetComponentInChildren<Text>().text = "Level - " + zoneNumber + "." + i;
             if(saveManager.GetCurrentSave().IsLevelCompleted((zoneNumber-1)*6 + i)) {
                 levelButtons[i-1].GetComponentInChildren<Text>().color = new Color(0, 1, 0, 1);
             } else {
@@ -22,15 +33,39 @@ public class ZoneUI : MonoBehaviour {
     }
 
     public void ButtonPressBack() {
-        if(zoneNumber == 2) LevelSelectionUIManager.SetActiveCanvas(LevelSelectionUILayout.ZONE_1);
-        if(zoneNumber == 3) LevelSelectionUIManager.SetActiveCanvas(LevelSelectionUILayout.ZONE_2);
-        if(zoneNumber == 4) LevelSelectionUIManager.SetActiveCanvas(LevelSelectionUILayout.ZONE_3);
+        if(zoneNumber == 2) {
+            LevelSelectionUIManager.SetActiveCanvas(LevelSelectionUILayout.ZONE_1);
+            playerCamera.backgroundColor = new Color(0.2455351f, 0f, 0.3867925f, 0f);
+            particleMainModule.startColor = new Color(1f, 0f, 0.5132675f, 1f);
+        }
+        if(zoneNumber == 3) {
+            LevelSelectionUIManager.SetActiveCanvas(LevelSelectionUILayout.ZONE_2);
+            playerCamera.backgroundColor = new Color(0f, 0.03444649f, 0.245283f, 0f);
+            particleMainModule.startColor = new Color(0f, 0.9696946f, 1f, 1f);
+        }
+        if(zoneNumber == 4) {
+            LevelSelectionUIManager.SetActiveCanvas(LevelSelectionUILayout.ZONE_3);
+            playerCamera.backgroundColor = new Color(0f, 0.2641509f, 0.1088356f, 0f);
+            particleMainModule.startColor = new Color(0.3039824f, 1f, 0.1556604f, 1f);
+        }
     }
 
     public void ButtonPressNext() {
-        if(zoneNumber == 1) LevelSelectionUIManager.SetActiveCanvas(LevelSelectionUILayout.ZONE_2);
-        if(zoneNumber == 2) LevelSelectionUIManager.SetActiveCanvas(LevelSelectionUILayout.ZONE_3);
-        if(zoneNumber == 3) LevelSelectionUIManager.SetActiveCanvas(LevelSelectionUILayout.ZONE_4);
+        if(zoneNumber == 1) {
+            LevelSelectionUIManager.SetActiveCanvas(LevelSelectionUILayout.ZONE_2);
+            playerCamera.backgroundColor = new Color(0f, 0.03444649f, 0.245283f, 0f);
+            particleMainModule.startColor = new Color(0f, 0.9696946f, 1f, 1f);
+        }
+        if(zoneNumber == 2) {
+            LevelSelectionUIManager.SetActiveCanvas(LevelSelectionUILayout.ZONE_3);
+            playerCamera.backgroundColor = new Color(0f, 0.2641509f, 0.1088356f, 0f);
+            particleMainModule.startColor = new Color(0.3039824f, 1f, 0.1556604f, 1f);
+        }
+        if(zoneNumber == 3) {
+            LevelSelectionUIManager.SetActiveCanvas(LevelSelectionUILayout.ZONE_4);
+            playerCamera.backgroundColor = new Color(1f, 0.3480333f, 0f, 0f);
+            particleMainModule.startColor = new Color(0.9330316f, 1f, 0f, 1f);
+        }
     }
 
     public void ButtonPressExit() {
